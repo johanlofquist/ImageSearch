c0af230249538875332aa4047a949612dab7603a
Johan Löfquist <johan.lofquist@gmail.com>
2024-03-19 11:29:31 +0100

    added validation

---
+++ server/schemas/request.schema.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+const requestSchema = Joi.object({
+  userId: Joi.string().required(),
+  favorites: Joi.array().items({
+    imageUrl: Joi.string().required(),
+    title: Joi.string().required(),
+  }).required(),
+});
+
+module.exports = { requestSchema };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs").promises;
 const cors = require("cors");
+const { requestSchema } = require("./schemas/request.schema");
 
 const saveCustomer = async (body) => {
   const data = await fs.readFile("./db.json", "utf-8");
@@ -28,6 +29,13 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/api/images/save", async (req, res) => {
+  const { error } = requestSchema.validate(req.body, { abortEarly: false });
+  console.log(error);
+
+  if (error) {
+    return res.status(400).json(error);
+  }
+
   await saveCustomer(req.body);
   res.status(200).send(req.body);
 });
